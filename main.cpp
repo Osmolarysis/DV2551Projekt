22b10223d6fdedd03864f285d065c4945abacab3
Filip <filip.unger@hotmail.se>
2021-03-01 10:34:46 +0100

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <windows.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+
+#pragma comment (lib, "d3d12.lib")
+#pragma comment (lib, "DXGI.lib")
+#pragma comment (lib, "d3dcompiler.lib")
+
+template<class Interface>
+inline void SafeRelease(
+	Interface** ppInterfaceToRelease)
+{
+	if (*ppInterfaceToRelease != NULL)
+	{
+		(*ppInterfaceToRelease)->Release();
+
+		(*ppInterfaceToRelease) = NULL;
+	}
+}
+
+int main()
+{
+	return 0;
+}
