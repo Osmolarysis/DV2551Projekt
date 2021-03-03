d259b53c6e6c050115dcccbe6d9ffee2f18e348a
Filip <filip.unger@hotmail.se>
2021-03-03 13:17:20 +0100

    Window now moves and closes

--- main.cpp
+++ main.cpp
@@ -6,9 +6,14 @@ int CALLBACK main(_In_ HINSTANCE appInstance, _In_opt_ HINSTANCE preInstance, _I
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // Check for memory leaks
 
+	//Renderer
 	Renderer* renderer = Renderer::getInstance();
 
-	while (1) { //TODO: exit while loop in a good way
+	//Windows handler
+	MSG msg = { 0 };
+	bool mainLoop = true;
+
+	while (mainLoop) { //TODO: exit while loop in a good way - statestacks if we feelin fancy
 		//Update
 
 		//Begin frame
@@ -18,6 +23,18 @@ int CALLBACK main(_In_ HINSTANCE appInstance, _In_opt_ HINSTANCE preInstance, _I
 		//Execute list(s)
 
 		//Present
+
+		//Message handling
+		while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+			switch (msg.message) {
+			case WM_QUIT: {
+				mainLoop = false;
+				break;
+			}
+			}
+		}
 	}
 
 	printf("Exiting main...\n");
