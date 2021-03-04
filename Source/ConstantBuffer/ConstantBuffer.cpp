4135d56d914ae2bcd7c5c4bb0f90e1391e1c1f87
Simon Andreasson <sian17@student.bth.se>
2021-03-04 11:15:14 +0100

    Set and update data

--- Source/ConstantBuffer/ConstantBuffer.cpp
+++ Source/ConstantBuffer/ConstantBuffer.cpp
@@ -30,16 +30,41 @@ ConstantBuffer::ConstantBuffer(UINT elementSize)
 			nullptr,
 			IID_PPV_ARGS(&m_constantBufferResource[i])
 		);
-
 		if (hr != S_OK) {
 			printf("Error creating constant buffer");
+		}
+
+		hr = m_constantBufferResource[i]->Map(0, nullptr, reinterpret_cast<void**>(&m_mappedData[i]));
+		if (hr != S_OK) {
+			printf("Error mapping constant buffer");
 		}
 	}
 }
 
 ConstantBuffer::~ConstantBuffer()
 {
-	for (int i = 0; i < NUM_SWAP_BUFFERS; i++) {
+	for (int i = 0; i < NUM_SWAP_BUFFERS; i++) 
+	{
+		m_constantBufferResource[i]->Unmap(0, nullptr);
 		SafeRelease(m_constantBufferResource[i].GetAddressOf());
 	}
+}
+
+//Set data for both CB resources and keep it there.
+void ConstantBuffer::setData(const void* data)
+{
+	for (int i = 0; i < NUM_SWAP_BUFFERS; i++)
+	{
+		memcpy(&m_mappedData[i], data, m_bufferSize);
+	}
+}
+
+//Set data for current backbuffer index.
+void ConstantBuffer::updateData(const void* data, UINT currentBackBufferIndex)
+{
+	memcpy(&m_mappedData[currentBackBufferIndex], data, m_bufferSize);
+}
+
+void ConstantBuffer::bind()
+{
 }
