89949c3ca50c70ebbd3d311d6494902d521b95a2
Simon Andreasson <sian17@student.bth.se>
2021-03-15 13:30:56 +0100

    DT and avg FPS in title

--- Source/Utility/Timer.h
+++ Source/Utility/Timer.h
@@ -8,10 +8,12 @@ private:
 	double m_elapsedTime = 0.0;
 	double m_frameTimeSum = 0.0;
 	double m_frameCount = 0.0;
+	double m_averageFPS = 0.0;
 	std::chrono::steady_clock::time_point m_time;
 public:
 	static Timer* getInstance();
 	void update();
 	double getDt();
+	double getAverageFPS(int updateInterval); 
 	void reset();
 };
