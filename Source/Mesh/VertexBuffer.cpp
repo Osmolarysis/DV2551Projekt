d29cf78498cefd2b08b9b9cbb5ad303bc1593866
Lingaard <39531334+Lingaard@users.noreply.github.com>
2021-03-15 16:56:04 +0100

    Snyggade till VertexBuffer classen

--- Source/Mesh/VertexBuffer.cpp
+++ Source/Mesh/VertexBuffer.cpp
@@ -1,98 +1,83 @@
 #include "VertexBuffer.h"
 
-VertexBuffer::VertexBuffer(size_t size, size_t sizeIndexData) // may move content to own function that is called in setData
+// Helper functions, will probably move. Depending on demand, move to utility class/file
+ComPtr<ID3D12Resource2> makeBufferHeap(D3D12_HEAP_TYPE type, UINT64 size, LPCWSTR heapName)	
 {
-	m_totalSize = size; //in bytes
-	m_nrOfVertices = size / sizeof(Vertex);
-	m_nrOfIndices = sizeIndexData / sizeof(UINT16);
-
 	//Do initial stuff
-	auto hp = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
-
-	auto rd = CD3DX12_RESOURCE_DESC::Buffer(m_totalSize);
-
-	// create vertexbuffer
+	auto hp = CD3DX12_HEAP_PROPERTIES(type);
+	auto rd = CD3DX12_RESOURCE_DESC::Buffer(size);
+	ComPtr<ID3D12Resource2> resource;
+	// create heap
 	HRESULT hr = Renderer::getInstance()->getDevice()->CreateCommittedResource(
 		&hp,
 		D3D12_HEAP_FLAG_NONE,
 		&rd,
 		D3D12_RESOURCE_STATE_GENERIC_READ,
 		nullptr,
-		IID_PPV_ARGS(m_vertexBufferResource.GetAddressOf())
+		IID_PPV_ARGS(resource.GetAddressOf())
 	);
 	if (hr != S_OK) {
 		printf("Error creating vertex buffer\n");
 		exit(-1);
 	}
-
-	m_vertexBufferResource->SetName(L"vb heap");
-
-	// create index buffer
-	if (m_nrOfIndices > 0)
-	{
-		hr = Renderer::getInstance()->getDevice()->CreateCommittedResource(
-			&hp,
-			D3D12_HEAP_FLAG_NONE,
-			&rd,
-			D3D12_RESOURCE_STATE_GENERIC_READ,
-			nullptr,
-			IID_PPV_ARGS(m_indexBufferResource.GetAddressOf())
-		);
-		if (hr != S_OK) {
-			printf("Error creating vertex buffer\n");
-			exit(-1);
-		}
-
-		m_indexBufferResource->SetName(L"ib heap");
-	}
+	resource->SetName(heapName);
+	return resource;
 }
 
-
-VertexBuffer::~VertexBuffer()
+void setUploadHeapData(ComPtr<ID3D12Resource2> resource, const void* data, size_t size)
 {
-	SafeRelease(m_vertexBufferResource.GetAddressOf());
-}
-
-void VertexBuffer::setData(const void* data)	// size and offset is in bytes
-{
-	//This copies some data to the buffer.
 	void* dataBegin = nullptr;
 	D3D12_RANGE range = { 0, 0 };	// We do not intend to read from this resource on the CPU.
-	HRESULT hr = m_vertexBufferResource->Map(0, &range, &dataBegin);
+	HRESULT hr = resource->Map(0, &range, &dataBegin);
 	if (hr != S_OK) {
 		printf("Error mapping vertex buffer resources");
 		exit(-1);
 	}
-	memcpy((char*)dataBegin, (char*)data, m_totalSize);
-	m_vertexBufferResource->Unmap(0, nullptr);
+	memcpy((char*)dataBegin, (char*)data, size);
+	resource->Unmap(0, nullptr);
+}
 
+
+// Member functions
+VertexBuffer::VertexBuffer(size_t size, size_t sizeIndexData) // may move content to own function that is called in setData
+{
+	m_totalSize = size; //in bytes
+	m_nrOfVertices = size / sizeof(Vertex);
+	m_nrOfIndices = sizeIndexData / sizeof(UINT16);
+	m_totalSizeIndices = sizeIndexData;
+
+	// create vertexbuffer
+	m_vertexBufferResource = makeBufferHeap(D3D12_HEAP_TYPE_UPLOAD, size, L"vb heap");
 	//Prepare VB view to be used in bind.
 	m_vertexBufferView.BufferLocation = m_vertexBufferResource->GetGPUVirtualAddress();
 	m_vertexBufferView.StrideInBytes = UINT(sizeof(Vertex));
 	m_vertexBufferView.SizeInBytes = UINT(m_totalSize);
+
+	// create index buffer
+	if (m_nrOfIndices > 0)
+	{
+		m_indexBufferResource = makeBufferHeap(D3D12_HEAP_TYPE_UPLOAD, sizeIndexData, L"ib heap");
+		//Prepare IB view to be used in bind.
+		m_indexBufferView.BufferLocation = m_indexBufferResource->GetGPUVirtualAddress();
+		m_indexBufferView.SizeInBytes = UINT(m_totalSizeIndices);
+		m_indexBufferView.Format = DXGI_FORMAT_R16_UINT;
+	}
+}
+
+VertexBuffer::~VertexBuffer()
+{
+	SafeRelease(m_vertexBufferResource.GetAddressOf());
+	SafeRelease(m_indexBufferResource.GetAddressOf());
 }
 
 void VertexBuffer::setData(const void* data, const void* indices)
 {
-	// set vertexbuffer
-	setData(data);
+	setUploadHeapData(m_vertexBufferResource, data, m_totalSize);
 
-	// set indexbuffer
-		//This copies some data to the buffer.
-	void* dataBegin = nullptr;
-	D3D12_RANGE range = { 0, 0 };	// We do not intend to read from this resource on the CPU.
-	HRESULT hr = m_indexBufferResource->Map(0, &range, &dataBegin);
-	if (hr != S_OK) {
-		printf("Error mapping vertex buffer resources");
-		exit(-1);
+	if (m_nrOfIndices > 0 && indices != nullptr)
+	{
+		setUploadHeapData(m_indexBufferResource, indices, m_totalSizeIndices);
 	}
-	memcpy((char*)dataBegin, (char*)indices, m_nrOfIndices*sizeof(UINT64));
-	m_indexBufferResource->Unmap(0, nullptr);
-
-	//Prepare VB view to be used in bind.
-	m_indexBufferView.BufferLocation = m_indexBufferResource->GetGPUVirtualAddress();
-	m_indexBufferView.SizeInBytes = UINT(m_nrOfIndices*sizeof(UINT16));
-	m_indexBufferView.Format = DXGI_FORMAT_R16_UINT;
 }
 
 void VertexBuffer::bind()
