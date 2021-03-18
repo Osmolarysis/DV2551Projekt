8b4836ec45f107469b47dbc66526651277f69ee0
Simon Andreasson <sian17@student.bth.se>
2021-03-18 15:33:06 +0100

    Name changes and comments

--- Source/ConstantBuffer/ConstantBuffer.cpp
+++ Source/ConstantBuffer/ConstantBuffer.cpp
@@ -8,12 +8,12 @@ ConstantBuffer::ConstantBuffer(UINT bufferSize, UINT location)
 
 	UINT cbSizeAligned = (m_bufferSize + 255) & ~255;
 
-	D3D12_HEAP_PROPERTIES heapProperties = {};
-	heapProperties.Type = D3D12_HEAP_TYPE_UPLOAD;
-	heapProperties.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
-	heapProperties.CreationNodeMask = 1; //used when multi-gpu (we don't do that here)
-	heapProperties.VisibleNodeMask = 1; //used when multi-gpu (we don't do that here)
-	heapProperties.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+	D3D12_HEAP_PROPERTIES heapPropertiesUpload = {};
+	heapPropertiesUpload.Type = D3D12_HEAP_TYPE_UPLOAD;
+	heapPropertiesUpload.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+	heapPropertiesUpload.CreationNodeMask = 1; //used when multi-gpu (we don't do that here)
+	heapPropertiesUpload.VisibleNodeMask = 1; //used when multi-gpu (we don't do that here)
+	heapPropertiesUpload.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
 
 	D3D12_HEAP_PROPERTIES heapPropertiesDefault = {};
 	heapPropertiesDefault.Type = D3D12_HEAP_TYPE_DEFAULT;
@@ -31,11 +31,13 @@ ConstantBuffer::ConstantBuffer(UINT bufferSize, UINT location)
 	resourceDesc.SampleDesc.Count = 1;
 	resourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
 
+	HRESULT hr;
+
 	for (int i = 0; i < NUM_SWAP_BUFFERS; i++)
 	{
 		//Create upload buffer
-		HRESULT hr = renderer->getDevice()->CreateCommittedResource(
-			&heapProperties,
+		hr = renderer->getDevice()->CreateCommittedResource(
+			&heapPropertiesUpload,
 			D3D12_HEAP_FLAG_NONE,
 			&resourceDesc,
 			D3D12_RESOURCE_STATE_GENERIC_READ,
@@ -45,7 +47,7 @@ ConstantBuffer::ConstantBuffer(UINT bufferSize, UINT location)
 		if (hr != S_OK) {
 			printf("Error creating upload constant buffer");
 		}
-		m_uploadBufferResource[i]->SetName(L"cb upload");
+		m_uploadBufferResource[i]->SetName(L"CB upload");
 
 		//Create default buffer
 		hr = renderer->getDevice()->CreateCommittedResource(
@@ -59,14 +61,15 @@ ConstantBuffer::ConstantBuffer(UINT bufferSize, UINT location)
 		if (hr != S_OK) {
 			printf("Error creating default constant buffer");
 		}
-		m_constantBufferResource[i]->SetName(L"cb heap");
+		m_constantBufferResource[i]->SetName(L"CB default");
 
-
+		//Describe size and location of constant buffer
 		D3D12_GPU_VIRTUAL_ADDRESS cbAddress = m_constantBufferResource[i]->GetGPUVirtualAddress();
 		D3D12_CONSTANT_BUFFER_VIEW_DESC cbvDesc = {};
 		cbvDesc.BufferLocation = cbAddress;
 		cbvDesc.SizeInBytes = cbSizeAligned;
 
+		//Calculate where in the descriptor heap the CB should be placed.
 		auto handle = renderer->getCBDescriptorHeap(i)->GetCPUDescriptorHandleForHeapStart();
 		auto offset = renderer->getDevice()->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV) * m_location;
 		handle.ptr += offset;
@@ -89,25 +92,28 @@ void ConstantBuffer::setData(const void* data)
 {
 	Renderer* renderer = Renderer::getInstance();
 
+	//Describe the data that will be set
 	D3D12_SUBRESOURCE_DATA vbData = {};
 	vbData.pData = data;
 	vbData.RowPitch = m_bufferSize;
 	vbData.SlicePitch = vbData.RowPitch;
 
+	//Uses direct lists to set initial values. Use updateData() to set specific buffer and command list
 	for (int i = 0; i < NUM_CONSTANT_BUFFERS; i++) {
 		UpdateSubresources(renderer->getDirectCommandList(i), m_constantBufferResource[i].Get(), m_uploadBufferResource[i].Get(), 0, 0, 1, &vbData);
 		CD3DX12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition(m_constantBufferResource[i].Get(),
 			D3D12_RESOURCE_STATE_COPY_DEST,
-			D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER);
+			D3D12_RESOURCE_STATE_COMMON);
 		renderer->getDirectCommandList(i)->ResourceBarrier(1, &barrier);
 	}
 }
 
-//Set data for current backbuffer index using a given command list.
+//Set data for specific backbuffer index using a given command list.
 void ConstantBuffer::updateData(const void* data, UINT currentBackBufferIndex, ID3D12GraphicsCommandList* cmdList)
 {
 	Renderer* renderer = Renderer::getInstance();
 
+	//Describe the data that will be set
 	D3D12_SUBRESOURCE_DATA vbData = {};
 	vbData.pData = data;
 	vbData.RowPitch = m_bufferSize;
